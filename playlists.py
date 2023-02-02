5a72701918c9a8c1f68e821dd1d633a585489b32
Ian Main <ian@pandadatasystems.com>
2023-02-01 20:04:15 -0800

    Update playlists.py

--- playlists.py
+++ playlists.py
@@ -5,22 +5,21 @@
 # /_/    \__,_/_/ /_/\__,_/\__,_/  /_____/\__,_/\__/\__,_/   /____/\__, /____/\__/\___/_/ /_/ /_/____/
 #                                                                 /____/
 # Written By: Immain
-# Date Created: 1/31/2023
+# Date Created: 1/26/2023
 # Version: 1.0.0
-# Description: Downloads playlists from youtube and saves it as a mp4 file with the thumbnail as a jpg file in the same directory as the script. 
+# Description: Downloads playlists from youtube and saves it as a mp4 file with the thumbnail as a jpg file in the same$
+import pafy, re, wget, os, ffmpeg, pytube
 
-import pafy, re, wget, string, random, os, ffmpeg, pytube
-
-url = "https://www.youtube.com/watch?v=6omHDfHITZ4&list=PLXa7L1ovsDcMwMxwBF4vyh-RLYkFABlb2"
+url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=PLetg744TF10BrdPjaEXf4EsJ1wz6fyf95"
 
 # Get playlist Thumbnails
 playlist = pytube.Playlist(url)
 for video in playlist.videos:
+    videoTitle = video.title
     exp = "^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*"
     s = re.findall(exp,video.watch_url)[0][-1]
-    letters = string.ascii_uppercase + string.digits
     thumbnail = f"https://i.ytimg.com/vi/{s}/maxresdefault.jpg"
-    file = f"{s}-{( ''.join(random.choice(letters) for i in range(10)) )}.jpg"
+    file = f"{videoTitle}.jpg"
     wget.download(thumbnail)
     os.rename("maxresdefault.jpg", file)
     print(thumbnail)
@@ -28,6 +27,7 @@ for video in playlist.videos:
 # Get playlist videos
 for video in playlist.videos:
     result = pafy.new(video.watch_url)
+    videoTitleResult = result.title
     streams = result.streams
     for stream in streams:
         print(stream)
@@ -36,9 +36,5 @@ for video in playlist.videos:
     video_stream = ffmpeg.input(best_quality_video.url)
     audio_stream = ffmpeg.input(bestaudio.url)
     ffmpeg.output(video_stream, audio_stream, "output.mp4").run()
-    filestream = f"{s}-{( ''.join(random.choice(letters) for i in range(10)) )}-video.mp4"
+    filestream = f"{videoTitleResult}.mp4"
     os.rename("output.mp4", filestream)
-
-
-
-
